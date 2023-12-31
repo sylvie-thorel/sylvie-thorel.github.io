7c72ce482d5da294ea43d52b9911d617745b4a57
achtaitaipai <charles.cailleteau@gmail.com>
2023-12-31 18:58:42 +0100

    bigger trap target

--- src/data/site.js
+++ src/data/site.js
@@ -2,6 +2,6 @@ module.exports = {
   title: "Sylvie Thorel",
   language: "fr",
   url: "https://example.com",
-  baseUrl: "/sylvie-thorel",
+  // baseUrl: "/sylvie-thorel",
   description: "Ceci n'est pas un site.",
 };
