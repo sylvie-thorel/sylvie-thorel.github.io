bbe6ef3488d1a1896a36700e641f23afac9aea6e
achtaitaipai <charles.cailleteau@gmail.com>
2024-01-02 09:47:27 +0100

    hosturl with environment variables

--- src/data/site.js
+++ src/data/site.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "Sylvie Thorel",
   language: "fr",
-  hostUrl: "https://achtaitaipai.github.io",
+  hostUrl: process.env.HOSTURL ?? "",
   baseUrl: process.env.BASEURL ?? "",
   description: "Ceci n'est pas un site.",
 };
