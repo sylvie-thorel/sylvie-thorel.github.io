8a39e6a3a43b6cf0ccafc2e25b2e7c7b328b1e6d
achtaitaipai <charles.cailleteau@gmail.com>
2024-01-02 09:28:07 +0100

    seo: robot & sitemap

--- src/data/site.js
+++ src/data/site.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: "Sylvie Thorel",
   language: "fr",
+  hostUrl: "https://achtaitaipai.github.io",
   baseUrl: process.env.BASEURL ?? "",
   description: "Ceci n'est pas un site.",
 };
