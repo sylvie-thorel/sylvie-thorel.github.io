8d6a9b1ff7072f7f6b9d7667702bd5825483f048
achtaitaipai <charles.cailleteau@gmail.com>
2024-01-03 18:21:11 +0100

    baseurl for images

--- markdown.config.js
+++ markdown.config.js
@@ -16,6 +16,7 @@ module.exports = function (config) {
         widths: [400, 800],
         formats,
         outputDir: path.join(config.dir.output, "img"),
+        urlPath: process.env.URL ?? "",
       };
       const metadata = eleventyImage.statsSync(imgPath, imageOptions);
       eleventyImage(imgPath, imageOptions);
